7e6a416ad93064bfea74560ac594f9af42691935
Seve95 <severininicola95@gmail.com>
2019-05-17 14:36:09 +0200

    - TestAlgorithm works also with no source

--- testAlgorithm.py
+++ testAlgorithm.py
@@ -12,8 +12,18 @@ import os
 #arg 1: number fo map to test
 
 tot = int(sys.argv[1])
+found = False
 
 for i in range(0,tot):
-	currentMap = "skymap" + str(i) + ".fits"
-	currentModel = "sigma4-" + str(i) + ".xml"
-	os.system("python sourceCoordinates.py " + currentMap + " " + currentModel + " 1")
+	for file in os.listdir("."):
+		currentMap = "skymap" + str(i) + ".fits"
+		if file == currentMap:
+			currentModel = "sigma4-" + str(i) + ".xml"
+			os.system("python sourceCoordinates.py " + currentMap + " " + currentModel + " 1")
+			found = True
+			break
+	
+	if found == False:
+		currentMap = "skymapNS" + str(i) + ".fits"
+		currentModel = 'noSource.xml'
+		os.system("python sourceCoordinates.py " + currentMap + " " + currentModel + " 1")
